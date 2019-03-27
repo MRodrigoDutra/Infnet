ede8991ea0fa0b7ed99d245a0b4bc1a4317c66d2
Rodrigo Dutra <marcos.dutra@al.infnet.edu.br>
2019-03-26 17:28:20 -0700

    Question two was finalized

--- TP3/src/TP3Questao2.java
+++ TP3/src/TP3Questao2.java
@@ -1,20 +1,35 @@
 
+
 public class TP3Questao2 {
 
 	public static void main(String[] args) {
 		int list [] = {1,5,7,13,10,8,6}; //Lista de Entrada 
-		String saida [] = new String [7];
-		//System.out.println ("Hello mother fuckers");
+		int saida [] = new int [7];
 		for (int i=0; i < list.length; i++){
-			if((list[i] % 5)==0){
-				saida[i] = Integer.toString(list[i]);				
-			}else {
-				saida[i] = " ";
+				if((list[i] % 5)==0){
+					saida[i] = list[i];
+				}
+				
+				else {
+					saida[i] = 0;
+			    }
+		System.out.println("Entrada: " + list[i]);		
+		}
+		System.out.println("------------------------");
+		for(int j=0;j < saida.length; j++){ //loop de impressão ignorando os resultados zero
+			if(saida[j]!=0){
+				System.out.println("Saída: " + saida[j]);
+			}else{
+				continue;
 			}
-			
-			System.out.println("Entrada: "+ list[i] + "      Saída: " + saida[i] );
 		}
 		
-	}
-
+			
+			
+			
+	}	
+			
 }
+
+
+
