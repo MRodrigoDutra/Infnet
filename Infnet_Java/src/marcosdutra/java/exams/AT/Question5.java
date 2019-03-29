9ad84dbeb60e2e67f07c2917b2252a59103e42be
Rodrigo Dutra <marcos.dutra@al.infnet.edu.br>
2019-03-29 09:51:05 -0700

    question five was desenvolved

--- Infnet_Java/src/marcosdutra/java/exams/AT/Question5.java
+++ Infnet_Java/src/marcosdutra/java/exams/AT/Question5.java
@@ -1,10 +1,23 @@
 package marcosdutra.java.exams.AT;
+import java.util.Scanner;
 
 public class Question5 {
-
+	
 	public static void main(String[] args) {
-		System.out.println("It works!");
-
+		Scanner input = new Scanner(System.in);
+		try
+		{
+			System.out.println("POTENCIAÇÃO");
+			System.out.println("-----------------------");
+			System.out.println("Insira o valor de X: ");
+			double x = input.nextDouble();
+			System.out.println("Insira o valor de Y: ");
+			double y = input.nextDouble();
+			System.out.println("-----------------" + "\nx: " + x + "\ny: " + y + "\nResultado: "+ Math.pow(x,y)); 
+		}catch (ArithmeticException e) 
+		{
+			System.out.println("Números inválidos! \nAmbos os números não podem ser iguais a zero.\n Insira novamente!");
+		}
 	}
 
 }
