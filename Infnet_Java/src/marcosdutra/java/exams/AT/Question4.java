5bc22f7541a1efbc438ca7f058c6f0b790f4bbdb
Rodrigo Dutra <marcos.dutra@al.infnet.edu.br>
2019-03-29 07:55:25 -0700

    The question four is a result of factorial for the first twenty numbers

--- Infnet_Java/src/marcosdutra/java/exams/AT/Question4.java
+++ Infnet_Java/src/marcosdutra/java/exams/AT/Question4.java
@@ -1,10 +1,27 @@
 package marcosdutra.java.exams.AT;
 
+
 public class Question4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Question4 fat = new Question4();
+		for (int i=0 ;i<=20 ;i++)                //Estrutura de repetição iterativa para inserir os 20 primeiros números e calcular os seus fatoriais
+		{ 
+			System.out.println( i + "! : "+ fat.fatorial(i));
+			
+		}
 	}
 
+	public long fatorial (int number){
+		if(number == 0)
+		{
+			return 1;
+		}
+		else
+		{
+			return number * this.fatorial(number - 1);
+		}
+		
+	}
+	
 }
